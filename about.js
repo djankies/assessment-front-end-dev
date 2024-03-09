ee7922838dfcf81b3fa39014ce0553234c5f38b4
Daniel Jankowski <djank0113@gmail.com>
2024-03-08 23:10:31 -0500

    More styling

--- about.js
+++ about.js
@@ -1,3 +1,12 @@
+const duckSass = [
+    `Don't touch me there!!`,
+    `Woah hey there, slow down, buddy.`,
+    `That's so inappropriate!`,
+    `GET your hands off me 😡`,
+    `No touchy or no pizza. Pick one.`,
+    `Who the HELL do you think you are!`,
+];
+
 console.log("hello world");
 
 function handleSubmit(evt) {
@@ -7,11 +16,19 @@ function handleSubmit(evt) {
 
 function noTouchy(e) {
     e.preventDefault();
-	alert(`Don't touch me there!!`)
+    const sass = duckSass[Math.floor(Math.random() * duckSass.length)];
+    console.log(sass);
+    alert(sass);
 }
 
-let form = document.querySelector("#contact");
-let duckMan = document.querySelector("img");
+const form = document.querySelector("#contact");
+const duckMan = document.querySelector("img");
 
 duckMan.addEventListener("mouseover", noTouchy);
+
 form.addEventListener("submit", handleSubmit);
+
+for (let i = 0; i < 10; i++) {
+    console.log(atob("ICAgIF9fCl9fXyggbyk+ClwgPF8uICkKIGAtLS0nICAgCk9IIERVQ0sh"));
+    console.log('');
+}
